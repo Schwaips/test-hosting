7df38d124e4a85fc5cce1b31112c8505b5abe688
Charlie Bertrand <charlie.bertrand@live.com>
2022-05-18 19:10:15 +0200

    setup root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :articles # => index, show, new, create, edit, update, destroy
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # '/' => index des articles
+  root to: 'articles#index'
+
+  resources :articles, except: %i[index] # => index, show, new, create, edit, update, destroy
 end
